2c05d834fb54c934f0de7b6675cab1ac80b82ed8
eden <eden.tsarfaty@post.runi.ac.il>
2025-09-19 00:54:43 +0200

    Added theme selector functionality

---
+++ public/js/theme.js
@@ -0,0 +1,34 @@
+// const e = require("express");
+
+const selector = document.getElementById("theme-select")
+const presetTheme = localStorage.getItem("theme");
+
+if (presetTheme) {
+  selector.value = presetTheme;
+} else {
+  selector.value = "system";
+}
+
+selector.addEventListener("change", (e) => {
+  const choice = e.target.value;
+  applyTheme(choice);
+})
+
+
+function applyTheme(choice) {
+  document.documentElement.classList.remove("dark", "light", "system");
+  if (choice === "dark") {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  }
+
+  if (choice === "light") {
+    document.documentElement.classList.add("light");
+    localStorage.setItem("theme", "light");
+  }
+
+  if (choice === "system") {
+    document.documentElement.classList.add("system");
+    localStorage.removeItem("theme");
+  }
+}
