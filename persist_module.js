c1893a1f10327ab895ff6de8e68d9be86db4138f
eden <eden.tsarfaty@post.runi.ac.il>
2025-09-21 19:27:53 +0200

    Implemented all data appends and overwrite functions

--- persist_module.js
+++ persist_module.js
@@ -2,10 +2,11 @@ import fs from "fs/promises";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 
-// Build dataDir
+// Build dataDir path
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const dataDir = join(__dirname, "data");
+
 
 // Shared module-level variables
 let users = {};
@@ -24,6 +25,7 @@ async function readJSON(filename, fallback) {
       // if file is missing return fallback
       return fallback;
     }
+    console.log(err);
     throw err;
   }
 }
@@ -37,5 +39,105 @@ async function loadAll() {
   activityLog = await readJSON("activity_log.json", []);
 }
 
+async function addUser(username, password, vessel, phone, dateOfBirth, email) {
+    users[username] = {"password": password, "vessel": vessel, "phone": phone, "DoB": dateOfBirth, "email": email};
+    await saveUsers();
+}
+
+async function saveUsers () {
+    try {
+        await fs.writeFile(join(dataDir, "users.json"), JSON.stringify(users, null, 2), "utf-8");
+    }
+    catch(err) {
+        console.log("Error saving users.json:", err);
+        throw err;
+    }
+}
+
+async function addProduct(title, description, price, img_url) {
+    // Finds next ID , gaps are allowed
+    const ids = Object.keys(products).map(Number);
+    const maxId = ids.length > 0 ? Math.max(...ids) : 100;
+
+    //Assumes admin's responsiblity that the url isn't malicious or huge (DOS), doesn't download it locally
+    products[maxId] = { title, description, price, "image": img_url};
+    await saveProducts();
+}
+
+async function saveProducts () {
+    try {
+        await fs.writeFile(join(dataDir, "products.json"), JSON.stringify(products, null, 2), "utf-8");
+    }
+    catch(err) {
+        console.log("Error saving products.json:", err);
+        throw err;
+    }
+}
+
+async function updateCart (username, items) {
+    if (!carts[username]) {
+        carts[username] = [];
+    }
+
+    carts[username]=items;
+    await saveCarts();
+}
+
+async function saveCarts () {
+    try {
+        await fs.writeFile(join(dataDir, "carts.json"), JSON.stringify(carts, null, 2), "utf-8");
+    }
+    catch(err) {
+        console.log("Error saving carts.json:", err);
+        throw err;
+    }
+}
+
+
+async function logPurchase (username, items, date) {
+    if (!purchases[username]) {
+        purchases[username] = [];
+    }
+    
+    purchases[username].push({ items, date });
+    await savePurchases();
+}
+
+async function savePurchases () {
+    try {
+        await fs.writeFile(join(dataDir, "purchases.json"), JSON.stringify(purchases, null, 2), "utf-8");
+    }
+    catch(err) {
+        console.log("Error saving purchases.json:", err);
+        throw err;
+    }
+}
+
+async function recordActivity (datetime, username, action) {
+    activityLog.push({ datetime, username, action })
+    await saveActivities();
+}
+
+async function saveActivities () {
+    try {
+        await fs.writeFile(join(dataDir, "activity_log.json"), JSON.stringify(activityLog, null, 2), "utf-8");
+    }
+    catch(err) {
+        console.log("Error saving activity_log.json:", err);
+        throw err;
+    }
+}
+
+async function saveAll () {
+    await Promise.all([
+        saveUsers(),
+        saveProducts(),
+        saveCarts(),
+        savePurchases(),
+        saveActivities()
+    ]);
+}
+
 // Exports
-export { loadAll, users, products, carts, purchases, activityLog };
+export { loadAll, addUser, addProduct, updateCart, logPurchase, recordActivity, saveAll,
+    users, products, carts, purchases, activityLog };
