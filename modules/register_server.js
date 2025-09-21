fcda24a0b424829ce83ee3f5d8f64a9873a8b0c5
eden <eden.tsarfaty@post.runi.ac.il>
2025-09-22 00:05:36 +0200

    Added server side register functionality

---
+++ modules/register_server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import { addUser, recordActivity, users } from "../persist_module.js";
+
+const router = express.Router();
+
+function validatePassword(password) {
+  return (
+    password.length >= 8 &&
+    /[A-Z]/.test(password) &&
+    /[a-z]/.test(password) &&
+    /\d/.test(password) &&
+    /[!@#$%^&*]/.test(password)
+  );
+}
+
+router.post('/register', async (req, res) => {
+  const { username, password, vessel, phone, dateOfBirth, email } = req.body;
+  if (username === "") {
+    return res.status(400).send("Username can't be empty");
+  }
+  
+  if (users.hasOwnProperty(username)) {
+    return res.status(400).send(`Username ${username} taken`);
+  }
+
+  if (!validatePassword(password)) {
+    return res.status(400).send("Password does not meet requirements");
+  }
+
+  try {
+    await addUser(username, password, vessel, phone, dateOfBirth, email);
+  }
+  catch {
+    res.status(500).send("Please try again later");
+  }
+
+  recordActivity(new Date().toISOString(), username, "Created");
+  res.status(200).send("User created successfully");
+});
+
+
+export default router;
